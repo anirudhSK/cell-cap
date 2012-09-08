25ffd3cf3ef936456513a98453b56567735dd180
Anirudh Sivaraman <sk.anirudh@gmail.com>
2012-09-08 09:08:46 -0400

    Only files relevant to cellular capacity estimation.

---
+++ cell-capacity/codel-client.cc
@@ -0,0 +1,129 @@
+#include <string>
+#include <vector>
+#include <poll.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "delay-servo.hh"
+#include <iostream>
+using namespace std;
+
+uint32_t punch_nat_hole ( const Socket & lte_socket, const Socket::Address & server_address ,  uint32_t local_id ) {
+  /* 3 way handshake to punch hole, ACK-NAT it and exchange IDs */
+  uint32_t remote_id;
+  while(1) {
+    std::cout<<"Still to get an ACK-NAT, trying to punch a hole again \n";
+    char id_str[20]; 
+    /* TODO : Fix this. Setting this to even 10 seems to create a buffer overflow under O3 */ 
+    sprintf(id_str,"%d",local_id);
+    string hole_punch( "HOLE-PUNCH");
+    string id_string(id_str);
+    std::cout<<"Sending Hole punch string "<<hole_punch+id_string<<"\n";
+    lte_socket.send( Socket::Packet( server_address , hole_punch+id_string ) );
+
+    struct pollfd poll_fds[ 1 ];
+    poll_fds[ 0 ].fd = lte_socket.get_sock();
+    poll_fds[ 0 ].events = POLLIN;
+
+    struct timespec timeout;
+    timeout.tv_sec =  1 ;
+    timeout.tv_nsec = 0 ;
+    ppoll( poll_fds, 1, &timeout, NULL );
+
+    /* receive and ack the ACK-NAT */
+    if ( poll_fds[ 0 ].revents & POLLIN ) {
+      Socket::Packet ack(lte_socket.recv());
+      if ( ack.payload.substr(0,7) == "ACK-NAT" ) {
+        remote_id=atoi(ack.payload.substr(7).c_str());
+        std::cout<<"Successfully punched a hole and received an ACK-NAT from the server. Ack it \n";
+        lte_socket.send( Socket::Packet( server_address , "ACK-ACK" ) );
+        break;
+      }
+    }
+  }
+  return remote_id; 
+}
+
+double hread( uint64_t in )
+{
+  return (double) in / 1.e9;
+}
+
+int main( int argc , char* argv[] ) {
+  
+  if(argc<6) {
+   std::cout<<"Usage: ./codel-client local_data_ip local_feedback_ip server_ip data_interface feedback_interface \n";
+   exit(1);
+  }
+ 
+  /* get details from cmd line */ 
+  std::string local_data_ip((const char*)argv[1]);
+  std::string local_feedback_ip((const char*)argv[2]);
+  std::string server_ip((const char*)argv[3]);
+  std::string data_interface((const char*)argv[4]);
+  std::string feedback_interface((const char*)argv[5]);
+
+  Socket::Address server_data( server_ip , 9000 );
+  Socket::Address server_feedback( server_ip , 9001 );
+
+  /* Create and bind LTE socket on USB0 tethered to the phone */
+  Socket data_socket,feedback_socket;
+
+  data_socket.bind( Socket::Address( local_data_ip, 18000 ) );
+  data_socket.bind_to_device( data_interface );
+
+  feedback_socket.bind( Socket::Address( local_feedback_ip, 18001 ) );
+  feedback_socket.bind_to_device( feedback_interface );
+
+  /* Keep sending packets to the server until he acks that he got your packet. Otherwise nothing can run */
+  /* Use a unique to avoid confusing old zombies */ 
+  uint32_t local_id = (int) getpid() ^ rand(); 
+  uint32_t remote_id=punch_nat_hole(data_socket,server_data,local_id); 
+  remote_id=punch_nat_hole(feedback_socket,server_feedback,local_id); 
+
+  std::cout<<"Local ID "<<local_id<<" remote id "<<remote_id<<"\n";
+
+  DelayServoReceiver downlink_receiver("DOWN-RX",data_socket,feedback_socket,server_feedback,remote_id);
+  DelayServoSender uplink_sender("UP-TX",data_socket,feedback_socket,server_data,local_id);  
+
+  while ( 1 ) {
+    fflush( NULL );
+
+    /* possibly send packet */
+    downlink_receiver.tick();
+    uplink_sender.tick(); 
+    /* wait for incoming packet OR expiry of timer */
+    struct pollfd poll_fds[ 2 ];
+    poll_fds[ 0 ].fd = data_socket.get_sock();
+    poll_fds[ 0 ].events = POLLIN;
+
+    poll_fds[ 1 ].fd = feedback_socket.get_sock();
+    poll_fds[ 1 ].events = POLLIN;
+
+    struct timespec timeout;
+    uint64_t next_transmission_delay = std::min ( downlink_receiver.wait_time_ns() , uplink_sender.wait_time_ns()  );
+    timeout.tv_sec = next_transmission_delay / 1000000000;
+    timeout.tv_nsec = next_transmission_delay % 1000000000;
+    ppoll( poll_fds, 2, &timeout, NULL );
+
+    if ( poll_fds[ 0 ].revents & POLLIN ) {
+      Socket::Packet incoming( data_socket.recv() );
+      uint32_t* pkt_id=(uint32_t *)(incoming.payload.data());
+      if(*pkt_id==remote_id) { /* this is data */
+       Payload *contents = (Payload *) incoming.payload.data();
+       contents->recv_timestamp = incoming.timestamp;
+       downlink_receiver.recv(contents);
+      }
+    }
+    else if ( poll_fds[ 1 ].revents & POLLIN ) {
+      Socket::Packet incoming( feedback_socket.recv() );
+      uint32_t* pkt_id=(uint32_t *)(incoming.payload.data());
+      if (*pkt_id==local_id) {/* this is feedback */  
+       Feedback *feedback = (Feedback *) incoming.payload.data();
+       feedback->recv_timestamp = incoming.timestamp;
+       uplink_sender.recv(feedback);
+      }
+    }
+  }
+}
---
+++ cell-capacity/delay-servo.cc
@@ -0,0 +1,132 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <iostream>
+#include "delay-servo.hh"
+
+DelayServoSender::DelayServoSender( const std::string & s_name, const Socket & s_data_socket, const Socket & s_feedback_socket, const Socket::Address & s_target, uint32_t local_id )
+  : _name( s_name ), 
+    _data_socket( s_data_socket ),
+    _feedback_socket( s_feedback_socket ),
+    _target( s_target ),
+    _current_rate( BACKOFF_RATE ),
+    _packets_sent( 0 ),
+    _unique_id( local_id ),
+    _next_transmission( Socket::timestamp() ),
+    _last_transmission( _next_transmission )
+{
+  std::cout<<"Servo PARAMETERS \n";
+  std::cout<<"PACKET_SIZE = "<<PACKET_SIZE<<"\n";
+}
+
+DelayServoReceiver::DelayServoReceiver( const std::string & s_name, const Socket & s_data_socket, const Socket & s_feedback_socket, const Socket::Address & s_source, uint32_t remote_id )
+  : _name( s_name ), 
+    _data_socket( s_data_socket ),
+    _feedback_socket( s_feedback_socket ),
+    _source ( s_source) ,
+    _rate_estimator( MINIMUM_RX_RATE, 1000 ),
+    _packets_received( 0 ),
+    _unique_id( remote_id ),
+    _next_transmission( Socket::timestamp() ),
+    _last_transmission( _next_transmission ),
+    _last_stat(Socket::timestamp()),
+    _hist()
+{
+}
+uint64_t DelayServoSender::wait_time_ns( void ) const
+{
+  uint64_t cur_ts=Socket::timestamp();
+  if(_next_transmission > (cur_ts + 1e6) ) { 
+    /* if it's at least 1 ms in the future , else don't sleep */
+    return _next_transmission - cur_ts;
+  }
+  else return 0;
+}
+uint64_t DelayServoReceiver::wait_time_ns( void ) const
+{
+  uint64_t cur_ts=Socket::timestamp();
+  if(_next_transmission > (cur_ts + 1e6) ) {
+    /* if it's at least 1 ms in the future , else don't sleep */
+    return _next_transmission - cur_ts;
+  }
+  else return 0;
+}
+void DelayServoReceiver::recv( Payload* contents )
+{
+   /* This has to be a data packet  */
+   /* Update controlled delay estimate, 
+      Make sure to echo sender ID on ACK */
+  _packets_received++;
+  double current_rate=0; 
+  _rate_estimator.add_packet( *contents );
+  current_rate=_rate_estimator.get_rate();
+  _hist.packet_received( *contents , current_rate );
+  double loss_rate = (double) _hist.num_lost() / (double) _hist.max_rx_seq_no();  
+    printf( "%s seq = %d delay = %f recvrate = %f avg-latency at rx = %f Mbps = %f lost = %.5f%% arrivemilli = %ld pkts_received = %d \n",
+          _name.c_str(),
+          contents->sequence_number,
+          (double) ((int64_t)contents->recv_timestamp - (int64_t)contents->sent_timestamp) / 1.0e9,
+          _rate_estimator.get_rate(),
+          _rate_estimator.get_latency(),
+          _rate_estimator.get_rate() * PACKET_SIZE * 8.0 / 1.0e6,
+          loss_rate * 100,
+          contents->recv_timestamp / 1000000,
+          _packets_received );
+    _last_stat=Socket::timestamp();
+}
+
+void DelayServoReceiver::tick( void ) 
+{
+  /* Make a feedback packet with num_outstanding & current_rate 
+     Send once in 20 ms */
+  if ( Socket::timestamp() > _last_stat + 2e9 ) { 
+      /* 2 seconds since last recv, actively purge */
+      std::cout<<"Actively purging here \n";
+      _hist.active_purge(_hist.max_rx_seq_no() + 10);
+      _last_stat=Socket::timestamp();
+  } 
+  if ( Socket::timestamp() > _next_transmission ) /* 20 ms*/ {
+       Feedback feedback;
+       feedback.current_rate=_rate_estimator.get_rate();
+       feedback.current_latency=_rate_estimator.get_latency();
+       feedback.sender_id = _unique_id;
+       feedback.sent_timestamp=Socket::timestamp();
+       _feedback_socket.send(Socket::Packet(_source, feedback.str(sizeof(Feedback))));
+       _next_transmission=_next_transmission + 1.e6*20  ;
+  }
+}
+
+void DelayServoSender::tick( void )
+{
+    uint64_t interpacket_delay=1e9/_current_rate;
+    uint64_t now = Socket::timestamp();
+    if ( _next_transmission <= now ) {
+      /* Send packet */
+      Payload outgoing;
+      outgoing.sequence_number = _packets_sent++;
+      outgoing.sent_timestamp = Socket::timestamp();
+      outgoing.sender_id = _unique_id;
+      _data_socket.send( Socket::Packet( _target, outgoing.str( PACKET_SIZE ) ) );
+      _last_transmission = outgoing.sent_timestamp;
+      _next_transmission = _last_transmission + interpacket_delay;
+      std::cout<<_name<<" Transmitting in tick @ "<<now<<" with rate "<<1.0e9/interpacket_delay<<" packets per second \n";
+    }
+}
+
+void DelayServoSender::recv(Feedback* feedback) {
+   std::cout<<_name<<" feedback @ delay "<<((int64_t)feedback->recv_timestamp-(int64_t)feedback->sent_timestamp)/1.0e9<<" rx rate "<<feedback->current_rate<<" rx latency "<<feedback->current_latency<<"\n";
+   /* Now act on feedback */
+   if(feedback->current_latency >= 10.0) {
+    /* Don't allow the queues to grow over 15 seconds */
+    std::cout<<_name<<" Backing off \n";
+    _current_rate=BACKOFF_RATE; 
+    return ;
+   }
+   /* Check the above two against 4G as well */ 
+   else {
+    _current_rate=2*feedback->current_rate; /* To make sure the buffer is non empty */
+    _current_rate=(_current_rate<MINIMUM_TX_RATE)?MINIMUM_TX_RATE:_current_rate;
+    return ;
+   }
+}
